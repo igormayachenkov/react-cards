402ad39a6b5e1ed021808e31e2490a0bac0e16a2
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2023-02-13 21:00:50 +0300

    repository link added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ function App() {
     <div id="app" className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <span>React expandable cards example</span>
+        <div>
+          <div className='title'>React expandable cards example</div>
+          <div className='repo' >the repository: 
+            <a href="https://github.com/igormayachenkov/react-cards" target='blank'>
+              github:igormayachenkov/react-cards
+            </a>
+            </div>
+        </div>
       </header>
 
       <div id="layout-list">
